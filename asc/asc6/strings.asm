722cdf707756c2bdd4507e1fc7e658fd2d4f54b0
Andrei Bratu <bratu.andrei@outlook.com>
2018-11-08 16:41:59 +0200

    Added seminar work

---
+++ asc/asc6/strings.asm
@@ -0,0 +1,27 @@
+bits 32 ; assembling for the 32 bits architecture
+
+global start        
+
+extern exit
+import exit msvcrt.dll
+
+
+segment data use32 class=data
+    a dd 1, 2, 3, 4
+    len equ ($-a)/4
+
+
+segment code use32 class=code
+    start:
+        mov ecx, len
+        mov esi, a
+        mov ebx, 0
+        cld
+        
+        bucla:
+            lodsb
+            add ebx, eax
+            loop bucla
+        
+        push    dword 0
+        call    [exit]
---
+++ asc/asc6/strings2.asm
@@ -0,0 +1,31 @@
+bits 32 ; assembling for the 32 bits architecture
+
+global start        
+
+extern exit
+import exit msvcrt.dll
+
+; b[i] <- a[i]
+
+segment data use32 class=data
+    a dd 1, 2, 3, 4
+    len equ ($-a)/4
+    b times len dd 0
+    
+
+
+segment code use32 class=code
+    start:
+        cld
+        mov esi, a
+        mov edi, b
+        mov ecx, len
+        
+        qw:
+            ; lodsd ; eax <- [esi]
+            ; stosd ; [edi] <- eax
+            movsd
+            loop qw
+        
+        push    dword 0
+        call    [exit]
