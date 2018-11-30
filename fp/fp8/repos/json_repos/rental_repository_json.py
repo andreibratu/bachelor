5d57c916689bc2f83a79a1a5415f33d80d19ea93
Andrei Bratu <bratu.andrei@outlook.com>
2018-11-30 20:20:46 +0200

    Added json repositories

---
+++ fp/fp8/repos/json_repos/rental_repository_json.py
@@ -0,0 +1,32 @@
+from repos.repos.rental_repository import RentalRepository
+import jsonpickle
+import os
+
+
+class JsonRentalRepository(RentalRepository):
+
+    def __init__(self):
+
+        self._path = 'storage/rentals.json'
+
+        super().__init__()
+
+        if os.stat(self._path).st_size != 0:
+            with open(self._path, 'r') as f:
+                db = jsonpickle.decode(f.read(), keys=True)
+                self._rentals = db['objects']
+                self._counter = db['counter']
+                self._movie_stats_days = db['stats_days']
+                self._movie_stats_times = db['stats_times']
+
+
+    def __del__(self):
+
+        with open(self._path, 'w') as f:
+            db = {
+                'objects': self._rentals,
+                'counter': self._counter,
+                'stats_days': self._movie_stats_days,
+                'stats_times': self._movie_stats_times
+            }
+            f.write(jsonpickle.encode(db))
