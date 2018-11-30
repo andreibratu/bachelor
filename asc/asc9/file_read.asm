602e40c1d065f8e144a6be180c2e2bae728e478b
Andrei Bratu <bratu.andrei@outlook.com>
2018-11-30 17:21:53 +0200

    Added asc lab 8

---
+++ asc/asc9/file_read.asm
@@ -0,0 +1,33 @@
+bits 32
+
+global start        
+
+extern exit, fprintf, fclose, fopen
+import exit msvcrt.dll
+import fprintf msvcrt.dll
+import fclose msvcrt.dll
+import fopen msvcrt.dll
+
+
+segment data use32 class=data
+    filename db "cool.txt", 0
+    mode db "r", 0
+    file_descriptor dd 0
+    
+
+segment code use32 class=code
+    start:
+        push mode
+        push filename
+        call [fopen]
+        
+        mov [file_descriptor], eax
+        cmp eax, 0
+        
+        je end_label
+        
+        mov [file_descriptor], eax
+        
+        end_label:
+            push    dword 0
+            call    [exit]
---
+++ asc/asc9/file_write.asm
@@ -0,0 +1,35 @@
+bits 32
+
+global start        
+
+extern exit, fprintf, fclose, fopen
+import exit msvcrt.dll
+import fprintf msvcrt.dll
+import fclose msvcrt.dll
+import fopen msvcrt.dll
+
+segment data use32 class=data
+    filename db "dimitri.txt", 0
+    mode db "w", 0
+    file_descriptor dd 0
+    text  db "Cheeki breeki", 0
+
+
+segment code use32 class=code
+    start:
+        push mode
+        push filename
+        call [fopen]
+        
+        mov [file_descriptor], eax
+        
+        push text
+        push file_descriptor
+        call [fprintf]
+        
+        push dword [file_descriptor]
+        call [fclose]
+        add esp, 4        
+        
+        push    dword 0
+        call    [exit]
