04bdd829df9e92e7602b8f6d19db95a1d18e312c
Andrei Bratu <bratu.andrei@outlook.com>
2018-12-10 12:57:21 +0200

    Added asc10 hw

---
+++ asc/asc10/asc10-1.asm
@@ -0,0 +1,67 @@
+; A file name is given (defined in the data segment). Create a file with the given name, then 
+; read words from the keyboard and write those words in the file, until character '$' is read from the keyboard
+bits 32
+
+global start        
+
+extern exit, scanf, printf, fprintf, fopen, fclose
+import exit msvcrt.dll
+import scanf msvcrt.dll
+import printf msvcrt.dll
+import fprintf msvcrt.dll
+import fopen msvcrt.dll
+import fclose msvcrt.dll
+
+segment data use32 class=data
+    filename db "cool.txt", 0
+    message db "Please input a number ", 0
+    write_mode db "w", 0
+    string_mode db "%s", 0
+    descriptor dd 0
+    var db 0
+    
+
+segment code use32 class=code
+    start:
+        push dword write_mode
+        push dword filename
+        call [fopen]
+        add esp, 4*2
+        
+        cmp eax, 0
+        jz final
+        mov [descriptor], eax
+        
+        input_loop:
+            push dword message
+            push dword string_mode
+            call [printf]
+            add esp, 4*2
+            
+            push dword var
+            push dword string_mode
+            call [scanf]
+            add esp, 4*2
+            
+            cmp byte [var], '$'
+            jz close_file
+            
+            push dword var
+            push dword [descriptor]
+            call [fprintf]
+            add esp, 4*2
+            
+            push dword ' '
+            push dword [descriptor]
+            call [fprintf]
+            add esp, 4*2
+            
+            jmp input_loop
+        
+        close_file:
+            push dword [descriptor]
+            call [fclose]
+        
+        final:
+            push    dword 0
+            call    [exit]
---
+++ asc/asc10/asc10.asm
@@ -0,0 +1,37 @@
+; Two natural numbers a and b (a, b: dword, defined in the data segment) are given. 
+; Calculate a/b and display the quotient and the remainder in the following format: 
+; "Quotient = <quotient>, remainder = <remainder>". 
+; Example: for a = 23, b = 10 it will display: "Quotient = 2, remainder = 3".
+; The values will be displayed in decimal format (base 10) with sign
+
+bits 32
+
+global start        
+
+extern printf, exit
+import exit msvcrt.dll
+import printf msvcrt.dll
+
+segment data use32 class=data
+    a dq 47676869565
+    b dd 238
+    format db "Quotient = %d, Remainder = %d", 0
+    input db 0
+
+
+segment code use32 class=code
+    start:
+        push dword [a]
+        push dword [a+4]
+        pop edx
+        pop eax
+        idiv dword [b]
+        
+        push edx
+        push eax
+        push format   
+        call [printf]
+        add esp, 4*3
+        
+        push    dword 0
+        call    [exit]
