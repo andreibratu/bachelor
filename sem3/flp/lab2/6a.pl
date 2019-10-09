f891827a4dd050e2dc60a9db2bb8fa272690bfda
Andrei Bratu <bratu.andrei@outlook.com>
2019-10-09 23:58:32 +0300

    Added lab2 hw flp

---
+++ sem3/flp/lab2/6a.pl
@@ -0,0 +1,9 @@
+% Check if element E exists in given list
+exists(E, [E|_]).
+exists(_, []) :- false.
+exists(E, [_|T]):-
+    exists(E, T).
+
+% Check if given list is a set
+isSet([]).
+isSet([E|S]) :- not(exists(E, S)),  isSet(S).
