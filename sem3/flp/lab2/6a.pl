88c60e2c0293cb1232a60bbac5a16b983de234f8
Andrei Bratu <bratu.andrei@outlook.com>
2019-10-22 11:32:55 +0300

    Finished lab2 flp

--- sem3/flp/lab2/6a.pl
+++ sem3/flp/lab2/6a.pl
@@ -1,9 +1,11 @@
+% flow(i, i)
 % Check if element E exists in given list
 exists(E, [E|_]).
 exists(_, []) :- false.
 exists(E, [_|T]):-
     exists(E, T).
 
+% flow(i, i)
 % Check if given list is a set
 isSet([]).
 isSet([E|S]) :- not(exists(E, S)),  isSet(S).
--- sem3/flp/lab2/6b.pl
+++ sem3/flp/lab2/6b.pl
@@ -1,14 +1,7 @@
-countOccurences(_, [], 0).
-countOccurences(H, [H|T], N) :- countOccurences(H, T, N1), N is N1 + 1.
-countOccurences(E, [_|T], N) :- countOccurences(E, T, N).
-
-removeInstances(_, [], []).
-removeInstances(X, [X|T], S) :- removeInstances(X, T, S).
-removeInstances(X, [U|T], [U|S]) :- removeInstances(X, T, S).
-
-removeKInstances(_, [], _, 0).
+% flow(i, i, o, i)
+removeKInstances(_, [], [], _).
 removeKInstances(X, [X|T], [X|S], 0) :- removeKInstances(X, T, S, 0).
-removeKInstances(X, [X|T], S, N) :- removeKInstances(X, T, S, N1), N is N1+1.
+removeKInstances(X, [X|T], S, N) :- N1 is N-1, removeKInstances(X, T, S, N1).
 removeKInstances(X, [U|T], [U|S], N) :- removeKInstances(X, T, S, N).
 
 
